7fc166d80f12fd3c0c8ff1d903b3a2adb8133c62
sangeetha <jadamalsangeetha@gmail.com>
2023-10-22 20:16:51 +0530

    unary operator overloading

---
+++ unary_operator_overloading.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class test
+{
+    private:
+    int num;
+    public:
+    test():num(8){}
+    void operator++()
+    {
+        num=num+2;
+    }
+    void display()
+}
