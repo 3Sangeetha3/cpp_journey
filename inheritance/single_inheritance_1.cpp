87b4dd02d0be2f61ae4851e20f4a8341bd1b5ad5
sangeetha <jadamalsangeetha@gmail.com>
2023-10-28 16:45:43 +0530

    single inheritance

--- inheritance/single_inheritance_1.cpp
+++ inheritance/single_inheritance_1.cpp
@@ -26,7 +26,7 @@ class B : public A
             cout<<"Enter the value of B = ";
             cin>>b;
         }
-        void siaplay_B()
+        void display_B()
         {
             display_A();
             cout<<"The value of B = "<<b<<endl;
@@ -41,7 +41,66 @@ int main()
 {
     B obj;
     obj.Set_B();
-    obj.siaplay_B();
+    obj.display_B();
     obj.cal_product();
     return 0;
 }
+
+/*
+// Example: 
+
+#include<iostream> 
+using namespace std; 
+
+class A 
+{ 
+	protected: 
+	int a; 
+	
+	public: 
+		void set_A(int x) 
+		{ 
+			a=x;			 
+		} 
+	
+		void disp_A() 
+		{ 
+			cout<<endl<<"Value of A="<<a; 
+		} 
+}; 
+
+class B: public A 
+{ 
+	int b,p; 
+	
+	public: 
+		void set_B(int x,int y) 
+		{ 
+			set_A(x); 
+			b=y; 
+		} 
+		
+		void disp_B() 
+		{ 
+			disp_A(); 
+			cout<<endl<<"Value of B="<<b; 
+		} 
+		
+		void cal_product() 
+		{ 
+			p=a*b; 
+			cout<<endl<<"Product of "<<a<<" * "<<b<<" = "<<p; 
+		} 
+		
+}; 
+
+main() 
+{ 
+	B _b; 
+	_b.set_B(4,5); 
+	_b.cal_product(); 
+	
+	return 0; 
+}
+
+*/
